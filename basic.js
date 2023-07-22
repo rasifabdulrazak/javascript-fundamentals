ee2626a041a96e6f1b981c83b7a838eb2b2c0023
RasifMohammed <rasif@metrictreelabs.com>
2023-07-22 13:00:50 +0530

    First commit

---
+++ basic.js
@@ -0,0 +1,7 @@
+// Working of js
+// =============
+//=> Javascript is a synchronous single threaded langauge
+//=> Everything in javascript happens inside execution context
+//=> Execution context has 2 parts 
+    // =>1.Memory Component(varaible envronment :key:value pair)
+    //  =>2.Code component(thread of execution : run 1 line at a time)
